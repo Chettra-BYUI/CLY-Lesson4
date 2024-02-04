ce207c6ddf697279cc6cd8347357e314c4773866
Chettra Ly <Chettra@BYUI.edu>
2024-02-03 17:19:19 -0800

    update frontend

--- app.js
+++ app.js
@@ -28,15 +28,17 @@ app
     res.setHeader('Content-Type', 'application/json');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     next();
-  });
+  })
 
+ 
 
   // .use(bodyParser.json())
   // .use((req, res, next) => {
   //   res.setHeader('Access-Control-Allow-Origin', '*');
   //   next();
   // })
-  // .use('/', require('./routes'));
+ 
+  .use('/', require('./routes'));
 
 mongodb.initDb((err) => {
   if (err) {
