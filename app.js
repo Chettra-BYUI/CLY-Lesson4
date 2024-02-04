e3cca1ad4513c434157e8d46a8b8cd9763ef0440
Chettra Ly <Chettra@BYUI.edu>
2024-02-03 17:31:08 -0800

    frontend update

--- app.js
+++ app.js
@@ -6,20 +6,8 @@ const port = process.env.PORT || 8080;
 const app = express();
 
 app
-  // .use(bodyParser.json())
-  // .use((req, res, next) => {
-  //   res.setHeader('Access-Control-Allow-Origin', '*');
-  //   res.setHeader(
-  //     'Access-Control-Allow-Headers',
-  //     'Origin, X-Requested-With, Content-Type, Accept, Z-Key'
-  //   );
-  //   res.setHeader('Content-Type', 'application/json');
-  //   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  //   next();
-  // })
-
   .use(bodyParser.json())
-  app.use((req, res, next) => {
+  .use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader(
       'Access-Control-Allow-Headers',
@@ -30,14 +18,12 @@ app
     next();
   })
 
- 
 
   // .use(bodyParser.json())
   // .use((req, res, next) => {
   //   res.setHeader('Access-Control-Allow-Origin', '*');
   //   next();
   // })
- 
   .use('/', require('./routes'));
 
 mongodb.initDb((err) => {
